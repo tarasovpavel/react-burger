01e29d27dd9f413afaf8b3865b36cee96b33da60
tarasovpavel <122258808+tarasovpavel@users.noreply.github.com>
2023-01-15 21:51:16 +0300

    Add files via upload

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import logo from './logo.svg';
 import './App';
 import styles from './App.module.css';
@@ -7,27 +7,55 @@ import BurgerIngredients from '../BurgerIngredients/burgerIngredients';
 import BurgerConstructor from '../BurgerConstructor/burgerConstructor';
 import AppHeader from '../AppHeader/appHeader';
 import { BrowserRouter } from 'react-router-dom';
+import { dataPath } from '../../constant';
+
+
+export const userDetailContext = React.createContext({});
 
 
 function App() {
+  const path = '';
+  const [data, setData] = useState({});
+  const [isFetch, setIsFetch] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://norma.nomoreparties.space/api/ingredients')
+        .then(response => response.json())
+        .then(result => { setData(result); setIsFetch(true) })
+
+        .catch(e => {
+          console.log(e);
+          setData({});
+          setIsFetch(false);
+        });
+    }
+    fetchData();
+  }
+    , []);
+
+
+
+
   return (
+    <userDetailContext.Provider value={{ data} }>
+      <main className={`${styles.page} `}>
 
-    <main className={`${styles.page} `}>
-      <BrowserRouter>
-        <AppHeader />
+        <BrowserRouter>
+          <AppHeader />
 
-        < div className={styles.container} >
-          <div className={styles.container_div_left}>
-            <BurgerIngredients />
+          < div className={styles.container} >
+            <div className={styles.container_div_left}>
+              <BurgerIngredients />
+            </div>
+            <div className={styles.container_div_right}>
+              <BurgerConstructor />
+            </div>        
           </div>
-          <div className={styles.container_div_right}>
-            <BurgerConstructor />
-          </div>
-
-        </div>
-      </BrowserRouter>
-    </main>
-
+        </BrowserRouter>
+      </main>
+    </userDetailContext.Provider>
 
   );
 }
---
+++ src/constant.js
@@ -0,0 +1,1 @@
+export const dataPath = 'https://norma.nomoreparties.space/api/ingredients';
